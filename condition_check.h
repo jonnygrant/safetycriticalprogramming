35e85c5b0d704297f8922537bbcb76697cc1944f
Jonathan Grant <jg@jguk.org>
2023-11-19 23:54:48 +0000

    change condition_check to completely format the string at compile time

--- condition_check.h
+++ condition_check.h
@@ -7,10 +7,12 @@
  Rather like assert(), but it doesn't need to always call abort()
  Safety critical software can printf, or log, and return an error
  */
+#define _CHECK_HELPER(x) #x
+#define _CHECK_LINE_STRING(x) _CHECK_HELPER(x)
 #define condition_check(condition, action) \
     do { \
         if (!(condition)) { \
-            printf("condition_check: %s %s:%d\n", #condition, __FILE__, __LINE__); \
+            printf("condition_check: (" #condition ") " __FILE__ ":" _CHECK_LINE_STRING(__LINE__) "\n"); \
             action; \
         } \
     } while (0)
